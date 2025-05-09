6b79d8e6f5b17c3fd8af0f7f5c5c39c8f1fc943c
JVThomas <jthomas0209@gmail.com>
2025-05-09 14:04:35 -0400

    changing base on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JVThomas.github.io/',
+  base: '/',
   plugins: [react({
     jsxImportSource: '@emotion/react',
 })],
